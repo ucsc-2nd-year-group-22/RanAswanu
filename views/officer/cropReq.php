0cbd06d6e2694b5f851dd6baae8862a9586fb6ec
ddhanushka <dimuthudhanushka8@gmail.com>
2020-10-12 17:10:41 +0530

    Add modified filter-panel for cropReq view

--- views/officer/cropReq.php
+++ views/officer/cropReq.php
@@ -7,45 +7,62 @@
     <? endforeach; ?>
     </ul>
 </div>
+
 <div class="filter-panel">
-    <div class="pane1">
-        <ul>
-            <li id="sortByLbl">
-                <label for="radio">Sort by</label></li>
-            <li>
-                <label class="container">Acending
-                    <input type="radio" checked="checked" name="radio">
-                    <span class="checkmark"></span>
-                </label>
-            </li>
-            <li>
-                <label class="container">Decending
-                    <input type="radio" name="radio">
-                    <span class="checkmark"></span>
-                </label>
-            </li>
-        </ul>                    
-    </div>
-    <div class="pane2">
-        <!-- panel2                     -->
-    </div>
-    <div class="pane3">
-        <label for="filter4">Sort by</label>
-        <select id="filter4" name="filter4">
-            <option value="australia">Date</option>
-            <option value="canada">Amount</option>
-            <option value="usa">Time</option>
-        </select>                    
-    </div>
-    <div class="pane4"> 
-        <label for="filter5">Search by</label>
-        <select id="filter5" name="filter5">
-            <option value="australia">Crop</option>
-            <option value="canada">Area</option>
-            <option value="usa">Officer</option>
-        </select>                
-    </div>
-</div>
+                <div class="pane1">
+                    <div class="left">
+                        <label for="radio">Sort by</label>
+                    </div>
+                    <div class="right">
+                        <ul>
+                            <li>
+                                <label class="container">Acending
+                                  <input type="radio" checked="checked" name="radio">
+                                  <span class="checkmark"></span>
+                                </label>
+                            </li>
+                            <li>
+                                <label class="container">Decending
+                                  <input type="radio" name="radio">
+                                  <span class="checkmark"></span>
+                                </label>
+                            </li>
+                        </ul>                    
+                    </div>
+                </div>
+                <div class="pane2">
+                    <div class="search-container">
+                        <form action="#">
+                            <span class="left">
+                                <input type="text" placeholder="Search.." name="search">
+                            </span>
+                            <span class="right">
+                                <button type="submit">Submit</button>
+                            </span>
+                        </form>
+                      </div>
+                    <div class="right">
+
+                    </div>
+                </div>
+                <div class="pane3">
+                    <label for="filter4">Sort by</label>
+                    <select id="filter4" name="filter4">
+                        <option value="australia">Date</option>
+                        <option value="canada">Amount</option>
+                        <option value="usa">Time</option>
+                    </select>                    
+                </div>
+                <div class="pane4"> 
+                    <label for="filter5">Search by</label>
+                    <select id="filter5" name="filter5">
+                        <option value="australia">Crop</option>
+                        <option value="canada">Area</option>
+                        <option value="usa">Officer</option>
+                    </select>                
+                </div>
+            </div>
+
 
 <div class="main-table">
     <table>

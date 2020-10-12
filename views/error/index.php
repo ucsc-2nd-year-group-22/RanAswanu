16884c52e336017cacba5c64a7d1207d8538a094
ddhanushka <dimuthudhanushka8@gmail.com>
2020-10-12 16:11:04 +0530

    Fix  header('location: login') path

--- views/error/index.php
+++ views/error/index.php
@@ -1,1 +1,4 @@
-<?php echo $errMsg ?>
+
+<div class="" style="width:400px; color:var(--danger); margin:10% auto; text-align: center;">
+    <p style="font-size: 2em;"><?php echo $errMsg ?></p>
+</div>

3e98b4ad7d8b361fe5986f90e92a216137da9a99
thilina3045 <hbthilinadilshan123@gmail.com>
2020-10-22 20:19:15 +0530

    added farmer_model file

--- controllers/farmer.php
+++ controllers/farmer.php
@@ -27,6 +27,28 @@ class Farmer extends Controller {
     public function sellyourcrops($arg = false) {
         $this->view->rendor('farmer/sellyourcrops');
     }
+   
+    public function vendOffers($arg = false) {
+        $this->view->rendor('farmer/vendOffers');
+    }
+
+    //instert damage claim information to the database
+    public function create()
+    {
+        $data=array();
+
+        $data['username'] = $_POST['username'];
+        $data['dmgdate'] = $_POST['dmgdate'];
+        $data['province'] = $_POST['province'];
+        $data['district'] = $_POST['district'];
+        $data['gramasewa'] = $_POST['gramasewa'];
+        $data['address'] = $_POST['address'];
+        $data['estdmgarea'] = $_POST['estdmgarea'];
+        $data['waydmg'] = $_POST['waydmg'];
+        $data['details'] = $_POST['details'];
+       
+    }
+
     
     
 

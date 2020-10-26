1127edcb97fa9a5e529657de67d23894957947fe
ddhanushka <dimuthudhanushka8@gmail.com>
2020-10-26 06:13:07 +0530

    Update filter-panes of officer's section

--- views/farmer/farmerMng.php
+++ views/farmer/farmerMng.php
@@ -11,84 +11,69 @@
 
 <div class="tabContainer" id="tab1C">
     <h2>View all reegistered farmers</h2>
-    <div class="filter-panel">
-            <div class="pane1">
-                <div class="left">
-                    <label for="radio">Sort by</label>
-                </div>
-                <div class="right">
-                    <ul>
-                        <li>
-                            <label class="container">Acending
-                                <input type="radio" checked="checked" name="radio">
-                                <span class="checkmark"></span>
-                            </label>
-                        </li>
-                        <li>
-                            <label class="container">Decending
-                                <input type="radio" name="radio">
-                                <span class="checkmark"></span>
-                            </label>
-                        </li>   
-                    </ul>                    
-                </div>
-            </div>
-            <div class="pane2">
-                <div class="search-container">
-                    <form action="#">
-                        <span class="left">
-                            <input type="text" placeholder="Search.." name="search">
-                        </span>
-                        <span class="right">
-                            <button type="submit">Submit</button>
-                        </span>
-                    </form>
-                    </div>
-                <div class="right">
+    <div class="panel-container">
+    <div class="pane1">
 
-                </div>
-            </div>
-            <div class="pane3">
-                <label for="filter4">Sort by</label>
-                <select id="filter4" name="filter4">
-                    <option value="australia">Date</option>
-                    <option value="canada">Amount</option>
-                    <option value="usa">Time</option>
-                </select>                    
-            </div>
-            <div class="pane4"> 
-                <label for="filter5">Search by</label>
-                <select id="filter5" name="filter5">
-                    <option value="australia">Crop</option>
-                    <option value="canada">Area</option>
-                    <option value="usa">Officer</option>
-                </select>                
-            </div>
-        </div>
-        <div class="main-table">
-            <table>
-                <tr>
-                    <th>#</th>
-                    <th>Officer-ID</th>
-                    <th>First Name</th>
-                    <th>Last name</th>
-                    <th>Action</th>
-                    <th>Cotact</th>
-                    <th>email</th>
-                </tr>
-            <?php $i = 0; foreach($officerData as $officer) :; $i++;?>
-                <tr>
-                    <td> <?=  $i ?></td>
-                    <td><?= $officer['id'];?> </td>
-                    <td><?= $officer['firstname'];?> </td>
-                    <td><?= $officer['lastname'];?> </td>
-                    <td><?= $officer['nic'];?> </td>
-                    <td><?= $officer['tel'];?> </td>
-                    <td><?= $officer['email'];?> </td>
-                </tr>
-            <?php endforeach;?>
-            </table>
-        </div>
+        <form class="search-bar">
+            <label>Search crop requests by : </label>
+            <select placeholder="Search ...">
+                    <option>Demand status</option>
+                    <option>Farmer name</option>
+                    <option>Crop</option>
+                </select>
+            <input type="text" placeholder="Search ...">
+            <button type="submit"><i class="fas fa-search"></i></button>
+        </form>
+
+    </div>
+    <div class="pane2">
+        <form class="normal-select">
+            <label>Sort crop requests by : </label>
+            <select placeholder="other">
+                    <option>Date</option>
+                    <option>Demand status</option>
+                    <option>Farmer name</option>
+                    <option>Crop</option>
+                <option>111</option>
+            </select>
+            <button type="submit" class="half"><i class="fas fa-sort-amount-down-alt"></i> Smaller-first </button>
+            <button type="submit" class="half"><i class="fas fa-sort-amount-down"></i> Larger-first</button>
+        </form>
+    </div>
+
+    <!-- Comment pane 3 & 4 If they are empty -->
+
+    <div class="pane3">
+         <label>Empty pane</label>
+    </div>
+    <div class="pane4">
+        <label>Empty pane</label>
+    </div>
+</div> 
+    <div class="main-table">
+        <table>
+            <tr>
+                <th>#</th>
+                <th>Officer-ID</th>
+                <th>First Name</th>
+                <th>Last name</th>
+                <th>Action</th>
+                <th>Cotact</th>
+                <th>email</th>
+            </tr>
+        <?php $i = 0; foreach($officerData as $officer) :; $i++;?>
+            <tr>
+                <td> <?=  $i ?></td>
+                <td><?= $officer['id'];?> </td>
+                <td><?= $officer['firstname'];?> </td>
+                <td><?= $officer['lastname'];?> </td>
+                <td><?= $officer['nic'];?> </td>
+                <td><?= $officer['tel'];?> </td>
+                <td><?= $officer['email'];?> </td>
+            </tr>
+        <?php endforeach;?>
+        </table>
+    </div>
     </div>
 
 
@@ -96,60 +81,46 @@
 
 <div id="tab2C" class="tabContainer">
     <h2>You are going to update farmers</h2>
-    <div class="filter-panel">
-        <div class="pane1">
-            <div class="left">
-                <label for="radio">Sort by</label>
-            </div>
-            <div class="right">
-                <ul>
-                    <li>
-                        <label class="container">Acending
-                            <input type="radio" checked="checked" name="radio">
-                            <span class="checkmark"></span>
-                        </label>
-                    </li>
-                    <li>
-                        <label class="container">Decending
-                            <input type="radio" name="radio">
-                            <span class="checkmark"></span>
-                        </label>
-                    </li>
-                </ul>                    
-            </div>
-        </div>
-        <div class="pane2">
-            <div class="search-container">
-                <form action="#">
-                    <span class="left">
-                        <input type="text" placeholder="Search.." name="search">
-                    </span>
-                    <span class="right">
-                        <button type="submit">Submit</button>
-                    </span>
-                </form>
-                </div>
-            <div class="right">
+   
+    <div class="panel-container">
+    <div class="pane1">
 
-            </div>
-        </div>
-        <div class="pane3">
-            <label for="filter4">Sort by</label>
-            <select id="filter4" name="filter4">
-                <option value="australia">Date</option>
-                <option value="canada">Amount</option>
-                <option value="usa">Time</option>
-            </select>                    
-        </div>
-        <div class="pane4"> 
-            <label for="filter5">Search by</label>
-            <select id="filter5" name="filter5">
-                <option value="australia">Crop</option>
-                <option value="canada">Area</option>
-                <option value="usa">Officer</option>
-            </select>                
-        </div>
+        <form class="search-bar">
+            <label>Search crop requests by : </label>
+            <select placeholder="Search ...">
+                    <option>Demand status</option>
+                    <option>Farmer name</option>
+                    <option>Crop</option>
+                </select>
+            <input type="text" placeholder="Search ...">
+            <button type="submit"><i class="fas fa-search"></i></button>
+        </form>
+
     </div>
+    <div class="pane2">
+        <form class="normal-select">
+            <label>Sort crop requests by : </label>
+            <select placeholder="other">
+                    <option>Date</option>
+                    <option>Demand status</option>
+                    <option>Farmer name</option>
+                    <option>Crop</option>
+                <option>111</option>
+            </select>
+            <button type="submit" class="half"><i class="fas fa-sort-amount-down-alt"></i> Smaller-first </button>
+            <button type="submit" class="half"><i class="fas fa-sort-amount-down"></i> Larger-first</button>
+        </form>
+    </div>
+
+    <!-- Comment pane 3 & 4 If they are empty -->
+
+    <div class="pane3">
+         <label>Empty pane</label>
+    </div>
+    <div class="pane4">
+        <label>Empty pane</label>
+    </div>
+</div> 
    <!-- Table -->
     <div class="main-table">
         <table>
@@ -183,60 +154,47 @@
 <!-- Tab 2 Delete -->
 <div id="tab3C" class="tabContainer">
     <h2>Carefull ! You are going to delete farmers</h2>
-    <div class="filter-panel">
-        <div class="pane1">
-            <div class="left">
-                <label for="radio">Sort by</label>
-            </div>
-            <div class="right">
-                <ul>
-                    <li>
-                        <label class="container">Acending
-                            <input type="radio" checked="checked" name="radio">
-                            <span class="checkmark"></span>
-                        </label>
-                    </li>
-                    <li>
-                        <label class="container">Decending
-                            <input type="radio" name="radio">
-                            <span class="checkmark"></span>
-                        </label>
-                    </li>
-                </ul>                    
-            </div>
-        </div>
-        <div class="pane2">
-            <div class="search-container">
-                <form action="#">
-                    <span class="left">
-                        <input type="text" placeholder="Search.." name="search">
-                    </span>
-                    <span class="right">
-                        <button type="submit">Submit</button>
-                    </span>
-                </form>
-                </div>
-            <div class="right">
+    
+    <div class="panel-container">
+    <div class="pane1">
 
-            </div>
-        </div>
-        <div class="pane3">
-            <label for="filter4">Sort by</label>
-            <select id="filter4" name="filter4">
-                <option value="australia">Date</option>
-                <option value="canada">Amount</option>
-                <option value="usa">Time</option>
-            </select>                    
-        </div>
-        <div class="pane4"> 
-            <label for="filter5">Search by</label>
-            <select id="filter5" name="filter5">
-                <option value="australia">Crop</option>
-                <option value="canada">Area</option>
-                <option value="usa">Officer</option>
-            </select>                
-        </div>
+        <form class="search-bar">
+            <label>Search crop requests by : </label>
+            <select placeholder="Search ...">
+                    <option>Demand status</option>
+                    <option>Farmer name</option>
+                    <option>Crop</option>
+                </select>
+            <input type="text" placeholder="Search ...">
+            <button type="submit"><i class="fas fa-search"></i></button>
+        </form>
+
     </div>
+    <div class="pane2">
+        <form class="normal-select">
+            <label>Sort crop requests by : </label>
+            <select placeholder="other">
+                    <option>Date</option>
+                    <option>Demand status</option>
+                    <option>Farmer name</option>
+                    <option>Crop</option>
+                <option>111</option>
+            </select>
+            <button type="submit" class="half"><i class="fas fa-sort-amount-down-alt"></i> Smaller-first </button>
+            <button type="submit" class="half"><i class="fas fa-sort-amount-down"></i> Larger-first</button>
+        </form>
+    </div>
+
+    <!-- Comment pane 3 & 4 If they are empty -->
+
+    <div class="pane3">
+         <label>Empty pane</label>
+    </div>
+    <div class="pane4">
+        <label>Empty pane</label>
+    </div>
+</div> 
+      
    <!-- Table -->
     <div class="main-table">
         <table>
--- views/officer/damageClaims.php
+++ views/officer/damageClaims.php
@@ -7,6 +7,47 @@
     </ul>
 </div>
 
+<div class="panel-container">
+    <div class="pane1">
+
+        <form class="search-bar">
+            <label>Search crop requests by : </label>
+            <select placeholder="Search ...">
+                    <option>Demand status</option>
+                    <option>Farmer name</option>
+                    <option>Crop</option>
+                </select>
+            <input type="text" placeholder="Search ...">
+            <button type="submit"><i class="fas fa-search"></i></button>
+        </form>
+
+    </div>
+    <div class="pane2">
+        <form class="normal-select">
+            <label>Sort crop requests by : </label>
+            <select placeholder="other">
+                    <option>Date</option>
+                    <option>Demand status</option>
+                    <option>Farmer name</option>
+                    <option>Crop</option>
+                <option>111</option>
+            </select>
+            <button type="submit" class="half"><i class="fas fa-sort-amount-down-alt"></i> Smaller-first </button>
+            <button type="submit" class="half"><i class="fas fa-sort-amount-down"></i> Larger-first</button>
+        </form>
+    </div>
+
+    <!-- Comment pane 3 & 4 If they are empty -->
+
+    <div class="pane3">
+         <label>Empty pane</label>
+    </div>
+    <div class="pane4">
+        <label>Empty pane</label>
+    </div>
+</div> 
+
+
 <div id="tab1C" class="tabContainer">
     <div class="main-table">
         <table>

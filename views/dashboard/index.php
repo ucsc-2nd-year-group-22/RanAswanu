229281d8e531cf464869bd19a9df18a51e5319fc
ashanaka <jpashanshanaka@gmail.com>
2020-10-28 11:03:27 +0530

    dashboard tweaks

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,7 +1,5 @@
 
-<h1>Dashboard</h1>
-
-<h2>For all users</h2>
+<h1>Welcome To Dashboard</h1>
 
 <?php
  
@@ -59,6 +57,33 @@ catch(\PDOException $ex){
     print($ex->getMessage());
 }
 
+
+$dataPoints2 = array();
+//Best practice is to create a separate file for handling connection to database
+try{
+     // Creating a new connection.
+    // Replace your-hostname, your-db, your-username, your-password according to your database
+    $link = new \PDO(   'mysql:host=localhost;dbname=mvc;charset=utf8mb4', //'mysql:host=localhost;dbname=canvasjs_db;charset=utf8mb4',
+                        'root', //'root',
+                        '', //'',
+                        array(
+                            \PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                            \PDO::ATTR_PERSISTENT => false
+                        )
+                    );
+    
+    $handle = $link->prepare('select text as label, id as y from data'); 
+    $handle->execute(); 
+    $result = $handle->fetchAll(\PDO::FETCH_OBJ);
+        
+    foreach($result as $row){
+        array_push($dataPoints2, array("label"=> $row->label, "y"=> $row->y));
+    }
+    $link = null;
+}
+catch(\PDOException $ex){
+    print($ex->getMessage());
+}
 ?>
 
 
@@ -85,7 +110,7 @@ window.onload = function() {
 
     var chart1 = new CanvasJS.Chart("chartContainer1", {
         animationEnabled: true,
-        theme: "light2",
+        theme: "light5",
         title:{
             text: "Registered Data"
         },
@@ -99,11 +124,33 @@ window.onload = function() {
         }]
     });
     chart1.render();
+
+    var chart2 = new CanvasJS.Chart("chartContainer2", {
+        animationEnabled: true,
+        theme: "light3",
+        title:{
+            text: "Crop Distribution"
+        },
+        axisY: {
+            title: "Harvest Per Land"
+        },
+        axisX:{
+            title: "Crop Name"
+        },
+        data: [{
+            type: "column",
+            yValueFormatString: "#,##0.## tonnes",
+            dataPoints: <?php echo json_encode($dataPoints1, JSON_NUMERIC_CHECK); ?>
+        }]
+    });
+    chart2.render();
  
 }
 </script>
-<div id="chartContainer" style="height: 370px; width: 100%; margin: 20px"></div>
-<div id="chartContainer1" style="height: 370px; width: 100%; margin: 20px"></div>
-<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 
-<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam nisi quaerat architecto sint explicabo atque ratione unde consequatur repudiandae eum, harum excepturi laborum id doloremque animi laboriosam earum odio accusantium.</p>
+
+<div id="chartContainer"></div>
+<div id="chartContainer1"></div>
+<div id="chartContainer2"></div>
+
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>

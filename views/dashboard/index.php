9591be02197672e4e756d690fcd27445bcd2f5ea
ashanaka <jpashanshanaka@gmail.com>
2020-10-30 22:57:19 +0530

    dashboard grid including

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,5 +1,5 @@
 
-<h1>Welcome To Dashboard</h1>
+<!-- <h1>Welcome To Dashboard</h1> -->
 
 <?php
  
@@ -84,6 +84,33 @@ try{
 catch(\PDOException $ex){
     print($ex->getMessage());
 }
+
+$dataPoints3 = array();
+//Best practice is to create a separate file for handling connection to database
+try{
+     // Creating a new connection.
+    // Replace your-hostname, your-db, your-username, your-password according to your database
+    $link = new \PDO(   'mysql:host=localhost;dbname=mvc;charset=utf8mb4', //'mysql:host=localhost;dbname=canvasjs_db;charset=utf8mb4',
+                        'root', //'root',
+                        '', //'',
+                        array(
+                            \PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                            \PDO::ATTR_PERSISTENT => false
+                        )
+                    );
+    
+    $handle = $link->prepare('select text as label, id as y from data'); 
+    $handle->execute(); 
+    $result = $handle->fetchAll(\PDO::FETCH_OBJ);
+        
+    foreach($result as $row){
+        array_push($dataPoints3, array("label"=> $row->label, "y"=> $row->y));
+    }
+    $link = null;
+}
+catch(\PDOException $ex){
+    print($ex->getMessage());
+}
 ?>
 
 
@@ -144,13 +171,36 @@ window.onload = function() {
         }]
     });
     chart2.render();
+
+    var chart3 = new CanvasJS.Chart("chartContainer3", {
+        animationEnabled: true,
+        theme: "light3",
+        title:{
+            text: "Crop Distribution"
+        },
+        axisY: {
+            title: "Harvest Per Land"
+        },
+        axisX:{
+            title: "Crop Name"
+        },
+        data: [{
+            type: "spline",
+            yValueFormatString: "#,##0.## tonnes",
+            dataPoints: <?php echo json_encode($dataPoints3, JSON_NUMERIC_CHECK); ?>
+        }]
+    });
+    chart3.render();
  
 }
 </script>
 
+<div class="charts">
+    <div id="chartContainer"></div>
+    <div id="chartContainer1"></div>
+</div>
 
-<div id="chartContainer"></div>
-<div id="chartContainer1"></div>
 <div id="chartContainer2"></div>
+<div id="chartContainer3"></div>
 
 <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>

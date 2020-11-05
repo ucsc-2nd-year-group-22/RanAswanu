898df1f4b664881bf74c1aea6cfd2871ebb803d2
ddhanushka <dimuthudhanushka8@gmail.com>
2020-11-05 23:21:37 +0530

    RestPw view improvements

--- views/auth/resetPw.php
+++ views/auth/resetPw.php
@@ -1,5 +1,5 @@
-<h2> Reset Password </h2>
-
+<h2 style="text-align:center;"> Reset your password </h2>
+<p style="text-align:center;padding-bottom:20px;"> An e-mail will be send to you with instructions on how to reset your password. </p>
 <!-- This is not good. Better to use sessions or jquery for the validation -->
 <!-- alert box -->
 <?php if(isset($_GET['reset'])): ?>
@@ -19,14 +19,18 @@
 <?endif; ?>
 <!-- end of alert -->
 
+
 <div class="main-form">
     <form action="<?=URL;?>auth/resetRq" method="post">
+        <div class="row">
+            
+        </div>
         <div class="row">
             <div class="col-25">
                 <label for="fname">Email address</label>
             </div>
             <div class="col-75">
-                <input type="email" id="email" name="email" placeholder="yourmail@org.com">
+                <input type="email" id="email" name="email" placeholder="Enter your e-mail address  ">
             </div>
         </div>
         <div class="row">

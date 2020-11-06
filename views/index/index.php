a96db1c1420c17b91ea1292f1f4a67796c135f07
ashanaka <jpashanshanaka@gmail.com>
2020-11-06 10:55:12 +0530

    discription card

--- views/index/index.php
+++ views/index/index.php
@@ -5,7 +5,10 @@
         <hr>
     </div>
     <div class="card-body">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi in reiciendis voluptas atque voluptate tenetur ratione dolorem recusandae vitae! Eos laboriosam qui error fugit, repudiandae dolor repellat eligendi corporis delectus!</p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi in reiciendis voluptas atque voluptate tenetur ratione dolorem recusandae vitae! Eos laboriosam qui error fugit, repudiandae dolor repellat eligendi corporis delectus!
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe laborum animi officia vitae cum quis quisquam id? Cum ex unde voluptate voluptas dicta harum distinctio deleniti eaque in? Magnam, pariatur?
+        </p>
         <a class="btn" href="<?php echo URL . 'user/register' ?>">Register as Vendor</a>
     </div>
 </div>

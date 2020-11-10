79b14e2feb4c0048f60adfa486546f472f2a930f
ddhanushka <dimuthudhanushka8@gmail.com>
2020-11-10 20:09:45 +0530

    Login username show remove theree dots

--- models/user_model.php
+++ models/user_model.php
@@ -109,7 +109,7 @@ class User_Model extends Model {
                     break;
                 
                 case 'farmer':
-                    header('location: ' . URL . 'farmer/');
+                    header('location: ' . URL . 'farmer/sellyourcrops');
                     break;
 
                 case 'vendor':

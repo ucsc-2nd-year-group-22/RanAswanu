15a1b945a0ac59744310a17b9546a354e9e85ee0
ddhanushka <dimuthudhanushka8@gmail.com>
2020-11-12 10:13:47 +0530

    Homepage grid

--- views/index/index.php
+++ views/index/index.php
@@ -1,6 +1,35 @@
 <div class="homecontainer">
 
-    
+    <div class="halfleft">
+        halfleft
+    </div>
+
+    <div class="halfright">
+        <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae deserunt, fugiat quam sequi et quae minus assumenda suscipit enim iure ullam, inventore magni, incidunt impedit rerum necessitatibus! Dolores cing elit. Vitae deserunt, fugiat quam sequi et quae minus assumenda suscipit enim iure ullam, inventore magni, incidunt impedit rerum necessitatibus! Dolores similique repellat recusandae ea architecto aliquid aut, omnis ab laborum cupiditate quidem quos quae asperiores illo, pariatur quis eius quibusdam nemo accusamus?cing elit. Vitae deserunt, fugiat quam sequi et quae minus assumenda suscipit enim iure ullam, inventore magni, incidunt impedit rerum necessitatibus! Dolores similique repellat recusandae ea architecto aliquid aut, omnis ab laborum cupiditate quidem quos quae asperiores illo, pariatur quis eius quibusdam nemo accusamus?cing elit. Vitae deserunt, fugiat quam sequi et quae minus assumenda suscipit enim iure ullam, inventore magni, incidunt impedit rerum necessitatibus! Dolores similique repellat recusandae ea architecto aliquid aut, omnis ab laborum cupiditate quidem quos quae asperiores illo, pariatur quis eius quibusdam nemo accusamus?similique repellat recusandae ea architecto aliquid aut, omnis ab laborum cupiditate quidem quos quae asperiores illo, pariatur quis eius quibusdam nemo accusamus?
+    </p>
+        <a class="btn" href="<?php echo URL . 'user/register' ?>">Register as Vendor</a>
+    </div>
+
+    <div class="quater1">
+        q1
+    </div>
+
+    <div class="quater2">
+        q2
+    </div>
+
+    <div class="quater3">
+        q3
+    </div>
+
+    <div class="quater4">
+        q4
+    </div>
+
+    <div class="wide">
+        wide
+    </div>
+
 </div>
 
-<a class="btn" href="<?php echo URL . 'user/register' ?>">Register as Vendor</a>

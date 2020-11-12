b59b8ed02745f8a6c1a8e627ab006f6252cebd23
ddhanushka <dimuthudhanushka8@gmail.com>
2020-11-12 11:13:44 +0530

    hompage css

--- views/index/index.php
+++ views/index/index.php
@@ -1,14 +1,21 @@
 <div class="homecontainer">
 
     <div class="halfleft">
-        halfleft
+        <div class="text-container">
+            Login to the system from here. If you are not registered please register now. (Note that only vendor can register via the form)
+            
+                <a class="reg" href="<?php echo URL . 'user/login' ?>">Login</a>
+
+        </div>   
     </div>
 
-    <div class="halfright">
-        <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae deserunt, fugiat quam sequi et quae minus assumenda suscipit enim iure ullam, inventore magni, incidunt impedit rerum necessitatibus! Dolores cing elit. Vitae deserunt, fugiat quam sequi et quae minus assumenda suscipit enim iure ullam, inventore magni, incidunt impedit rerum necessitatibus! Dolores similique repellat recusandae ea architecto aliquid aut, omnis ab laborum cupiditate quidem quos quae asperiores illo, pariatur quis eius quibusdam nemo accusamus?cing elit. Vitae deserunt, fugiat quam sequi et quae minus assumenda suscipit enim iure ullam, inventore magni, incidunt impedit rerum necessitatibus! Dolores similique repellat recusandae ea architecto aliquid aut, omnis ab laborum cupiditate quidem quos quae asperiores illo, pariatur quis eius quibusdam nemo accusamus?cing elit. Vitae deserunt, fugiat quam sequi et quae minus assumenda suscipit enim iure ullam, inventore magni, incidunt impedit rerum necessitatibus! Dolores similique repellat recusandae ea architecto aliquid aut, omnis ab laborum cupiditate quidem quos quae asperiores illo, pariatur quis eius quibusdam nemo accusamus?similique repellat recusandae ea architecto aliquid aut, omnis ab laborum cupiditate quidem quos quae asperiores illo, pariatur quis eius quibusdam nemo accusamus?
-    </p>
-        <a class="btn" href="<?php echo URL . 'user/register' ?>">Register as Vendor</a>
+    <div class="halfright">     
+        <div class="text-container">
+            If you are a buyer who whish to buy the quality crops from our farmers please register now. We have a community of farmers more than 10,000.
+            
+                <a class="reg" href="<?php echo URL . 'user/register' ?>">Register as Vendor</a>
+
+        </div>   
     </div>
 
     <div class="quater1">

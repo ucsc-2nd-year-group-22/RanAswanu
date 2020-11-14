6747d71ce50f324de633b1dbf80f18bcd9cd01a1
thilina3045 <hbthilinadilshan123@gmail.com>
2020-11-14 20:09:19 +0530

    Added required attribute(damageclaim)

--- views/farmer/damageclaim.php
+++ views/farmer/damageclaim.php
@@ -12,7 +12,7 @@
             <label for="dmgdate">Date That Damage Is Happend</label>
             </div>
             <div class="col-75">
-            <input type="date" id="dmgdate" name="dmgdate" placeholder="Month/Date/Year ">
+            <input type="date" id="dmgdate" name="dmgdate" placeholder="Month/Date/Year " required>
             </div>
         </div>
         
@@ -21,7 +21,7 @@
             <label for="province">Province</label>
             </div>
             <div class="col-75">
-            <select id="province" name="province">
+            <select id="province" name="province" required>
             <option value ="" disabled selected>select province</option>
                 <option value="province1">Province 1</option>
                 <option value="province2">Province 2</option>
@@ -34,7 +34,7 @@
             <label for="district">District</label>
             </div>
             <div class="col-75">
-            <select id="district" name="district">
+            <select id="district" name="district" required>
             <option value ="" disabled selected>select district</option>
                 <option value="district1">District 1</option>
                 <option value="district2">District 2</option>
@@ -47,7 +47,7 @@
             <label for="gramasewa">Gramasewa Division</label>
             </div>
             <div class="col-75">
-            <select id="gramasewa" name="gramasewa">
+            <select id="gramasewa" name="gramasewa" required>
             <option value ="" disabled selected>select gramasewa division</option>
                 <option value="grama1">Grama 1</option>
                 <option value="grama2">Grama 2</option>
@@ -60,7 +60,7 @@
             <label for="address">Address</label>
             </div>
             <div class="col-75">
-                <input type="text" id="address" name="address" placeholder="ex: No. 32, Atha watunu wava, Horawpathana">
+                <input type="text" id="address" name="address" placeholder="ex: No. 32, Atha watunu wava, Horawpathana" required >
             </div>
         </div>  
         
@@ -69,7 +69,7 @@
             <label for="estdmgarea">Estimated Damage Area -Acres</label>
             </div>
             <div class="col-75">
-            <input type="number" id="estdmgarea" name="estdmgarea" placeholder="ex: 3 acres"  max="100">
+            <input type="number" id="estdmgarea" name="estdmgarea" placeholder="ex: 3 acres"  max="100" required>
             </div>
         </div>
 
@@ -78,7 +78,7 @@
             <label for="waydmg">Way Of Damage</label>
             </div>
             <div class="col-75">
-            <textarea id="waydmg" name="waydmg" placeholder="Enter the way that damage is happend" style="height:200px"></textarea>
+            <textarea id="waydmg" name="waydmg" placeholder="Enter the way that damage is happend" style="height:200px" required></textarea>
             </div>
         </div>
 
@@ -87,7 +87,7 @@
             <label for="details">Details Of Damaged Crops</label>
             </div>
             <div class="col-75">
-            <textarea id="details" name="details" placeholder="crop type   -   area   -   crop amount" style="height:200px"></textarea>
+            <textarea id="details" name="details" placeholder="crop type   -   area   -   crop amount" style="height:200px" required></textarea>
             </div>
         </div>
      
@@ -96,7 +96,7 @@
             
             </div>
             <div class="col-75">
-            <input type="submit" value="Submit">
+            <input type="submit"  value="Submit" >
             </div>
         </div>
 

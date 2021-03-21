4e100855fce27619b22e67f3aca18b3c7955b72f
thilina3045 <hbthilinadilshan123@gmail.com>
2021-03-22 04:26:07 +0530

    Added display crops in Buycrops,Added profile details

--- controllers/vendor.php
+++ controllers/vendor.php
@@ -1,161 +1,78 @@
 <?php
 
-class Vendor extends Controller
-{
+class Vendor extends Controller{
 
-    function __construct()
-    {
+    public function __construct() {
         parent::__construct();
         Session::init();
+        $logged = Session::get('loggedIn');
+        $role = Session::get('role');
     }
 
-    function index()
-    {
+    function index() {
         $sellData = $this->model->cropDetails();
         $data['Req'] = $sellData;
 
         $myOffers = $this->model->myOffers(Session::get('id'));
         $data['myOffers'] = $myOffers;
-
+        
         // print_r($data);
-        $this->setActivePage('index');
-        $this->view->rendor('vendor/sellingReq', $data);
+        //$this->setActivePage('index');
+       // $this->view->rendor('vendor/sellingReq', $data);
+
     }
 
-    public function register($arg = false)
-    {
-        $this->view->rendor('vendor/register');
+    //Display Cropsmng
+    Public function allCrops(){
+        $this->setActivePage('allCrops');
+        $this->view->rendor('vendor/allCrops');
+
+    } 
+
+    //Display Offermng
+    public function OffersMng(){
+        $this->setActivePage('OffersMng');
+        $this->view->rendor('vendor/OffersMng');
     }
 
-    //retriev all vendors
-    public function vendors()
-    {
+    Public function viewfarmerprofile($user_id){
+        // $data=['user_id'=>$user_id];
+        $data['user_id']=$user_id;
+        
+        // $data['farmerprofiledata']=$this->model->viewprofile($user_id);
+        $ss=$this->view->vendr=$this->model->viewprofile($user_id);
+        $data['details']=$ss;
+        $this->view->rendor('vendor/viewfarmerprofile',$data);
+        //print_r($data['ccc']);
+    } 
 
-        $vendorData = $this->model->vendorList();
+    // public function countsellingreqid($user_id){
+    //     $data['user_id']=$user_id;
+        
+    //     // $data['farmerprofiledata']=$this->model->viewprofile($user_id);
+    //     $ss=$this->model->countsellingreq($user_id);
+    //     $data['sellingreq']=$ss;
+    //     $this->view->rendor('vendor/viewfarmerprofile',$data);
 
-        $pageData = [
-            'role' => Session::get('role'),
-            'tabs' => [],
-            'vendorData' => $vendorData,
-        ];
+    // }
 
-        $this->setActivePage('userMgt');
-        $this->view->rendor('vendor/vendors', $pageData);
-    }
 
-    //remove a vendor
-    public function delete($id)
-    {
-        $this->model->delete($id);
-        header('location: ' . URL . 'vendor/vendors');
-    }
-
-    //MY FUNCTIONS
-    public function placeaOffer($id)
-    {
-        $data['aId'] = $id;
-        $this->view->rendor('vendor/placeaOffer', $data);
-    }
-
-    public function offer($aId)
-    {
-        $data['Vid'] = Session::get('id');
-        $data['aId'] =  $aId;
-        $data['Ammount'] =  $_POST['ammount'];
-        $this->model->setOffer($data);
-        header('location: ' . URL . 'vendor/index');
-    }
-
-    public function updateOffer($id)
-    {
-        $data['reqid'] = $id;
-        $this->view->rendor('vendor/updateOffer', $data);
-    }
-
-    public function update($reqid)
-    {
-        $data['reqid'] =  $reqid;
-        $data['amount'] =  $_POST['ammount'];
-        $this->model->updateOffer($data);
-        header('location: ' . URL . 'vendor/index');
-    }
-
-    public function undoOffer($id)
-    {
-        $data['reqid'] = $id;
-        $this->model->undoOffer($data);
-        header('location: ' . URL . 'vendor/index');
-    }
-
-    public function viewFarmer($id)
-    {
-        $farmerData = $this->model->farmerDetail($id);
-        $data['Fdata'] = $farmerData;
-        $this->view->rendor('farmer/viewFarmer', $data);
-    }
-
-    public function viewCrops()
-    {
-
-        $crop =  [
-            [
-                'name' => "Orange",
-                'type' => "MICH-Y1",
-                'ammount' => "12",
-            ],
-            [
-                'name' => "Orange",
-                'type' => "MICH-Y1",
-                'ammount' => "12",
-            ],
-        ];
-
-        $Cropdata = ['Cdata' => $crop];
-        $this->view->rendor('farmer/viewCrops', $Cropdata);
-    }
-
-    //Search vendors by name
-    public function ajxSearchVendorName()
-    {
-
-        $d = $this->model->ajxSearchVendorName($_POST['search']);
-        $data['farmerData'] = $d;
-        // print_r($data['farmerData']);
+    //////////////////////////AJAX///////////////////
+    public function ajxListCrop() {
+        // $d = $this->model->ajxListCrop($_POST['harvest_id']);
+        $d = $this->model->ajxListCrop();
+        $data['crops'] = $d;
+      // print_r($data['crops']);
+        
         if (!empty($d)) {
-            $this->view->rendor('vendor/ajxVendorList', $data, $withoutHeaderFooter = true);
+            
+            $this->view->rendor('vendor/ajxCrop', $data,$withoutHeaderFooter = true);
         } else {
             $data['errMsg'] = "No Result Found !";
-            $this->view->rendor('error/index', $data, $withoutHeaderFooter = true);
+            $this->view->rendor('error/index', $data,$withoutHeaderFooter = true);
         }
     }
+    
 
-    //Search vendor by NIC 
-    public function ajxSearchVendorNic()
-    {
-        $d = $this->model->ajxSearchVendorNic($_POST['search']);
-        $data['farmerData'] = $d;
-        // print_r($data['farmerData']);
-        if (!empty($d)) {
-            $this->view->rendor('vendor/ajxVendorList', $data, $withoutHeaderFooter = true);
-        } else {
-            $data['errMsg'] = "No Result Found !";
-            $this->view->rendor('error/index', $data, $withoutHeaderFooter = true);
-        }
-    }
 
-    //Sort vendors
-    public function ajxFilterVendor()
-    {
-
-        $d = $this->model->ajxFilterVendor($_POST['filter'], $_POST['ascOrDsc']);
-        $data['farmerData'] = $d;
-
-        // print_r($data['farmerData']);
-        if (!empty($d)) {
-            $this->view->rendor('vendor/ajxVendorList', $data, $withoutHeaderFooter = true);
-        } else {
-            $data['errMsg'] = "No Result Found !";
-            $this->view->rendor('error/index', $data, $withoutHeaderFooter = true);
-        }
-    }
 }

46484cfbaa5ec8d0a998e94bc2ff712d9b5d102c
thilina3045 <hbthilinadilshan123@gmail.com>
2021-03-25 17:02:28 +0530

    Added some changes

--- views/vendor/ajxCrop.php
+++ views/vendor/ajxCrop.php
@@ -6,28 +6,35 @@
         <th>Crop Name</th>
         <th>Weight</th>
         <th>Price</th>
-        <th>District</th> 
+        <th>District</th>
         <th>Action</th>
-       <!-- <th>View Profile</th> -->
+        <!-- <th>View Profile</th> -->
     </tr>
 
 
-   
-    <?php $i=0;
-    foreach($crops as $cropsall):;
-    $i++;?>
-    <tr>
-        <td><?= $i ?> </td>
-        <td ><a class="icon-color" style="font-size:1.5em;" href="<?php echo URL . 'vendor/viewfarmerprofile/' . $cropsall['user_id'] ;?>"> <i class="far fa-address-card"></i></a></td>
-        <td><?= $cropsall['crop_type']; ?> </td>
-        <td><?= $cropsall['harvest_amount']; ?> </td>
-        <td><?= $cropsall['max_offer']; ?> </td>
-        <td><?= $cropsall['ds_name']; ?></td>
-        <td><a type="button" class="mini-button btn-success" href="<?php echo URL . '/vendor/giveOffer' . $cropsall['farmer_user_id'] ;?>"><i class="fa fa-money"></i> Offer</a></td>
-        <!-- <td style ="text-align:center;"><a class="icon-color"; style="font-size:1.5em;" href="<?php echo URL .'vendor/viewfarmerprofile/'.$cropsall['farmer_user_id'];?>"><i class="far -fa-address-card"></td> -->
-        <!-- <td ><a class="icon-color" style="font-size:1.5em;" href="<?php echo URL . 'vendor/viewfarmerprofile/' . $cropsall['user_id'] ;?>"> <i class="far fa-address-card"></i></a></td> -->
 
-    </tr>
+    <?php $i = 0;
+    foreach ($crops as $cropsall) :;
+        $i++; ?>
+        <tr>
+            <td><?= $i ?> </td>
+            <td><a class="icon-color" style="font-size:1.5em;" href="<?php echo URL . 'vendor/viewfarmerprofile/' . $cropsall['farmer_user_id']; ?>"> <i class="far fa-address-card"></i></a></td>
+            <td><?= $cropsall['crop_type']; ?> </td>
+            <td><?= $cropsall['harvest_amount']; ?> </td>
+            <td><?= $cropsall['max_offer']; ?> </td>
+            <td><?= $cropsall['ds_name']; ?></td>
+            <td>
+            <?php if ($cropsall['offer_sent'] == 1) : ?>
+                <a type="button" class="mini-button btn-success" href="<?php echo URL . 'vendor/giveOffer/' . $cropsall['selling_req_id'] . '/' . $cropsall['user_id']; ?>"><i class="fa fa-money"></i> Offer</a>
+                <!-- <a href="<?php echo URL . 'vendor/placeaOffer/' . $cropsall['selling_req_id'] ?>" class="mini-button normal">Offer</a> -->
+            <?php else : ?>
+                Offer already sent
+            <?php endif; ?>
+            </td>
 
-    <?php endforeach; ?>   
-</table> 
+            <!-- <td><a type="button" class="mini-button btn-success" href="<?php echo URL . 'vendor/giveOffer/' . $cropsall['selling_req_id'] . '/' . $cropsall['user_id']; ?>"><i class="fa fa-money"></i> Offer</a></td> -->
+
+        </tr>
+
+    <?php endforeach; ?>
+</table>

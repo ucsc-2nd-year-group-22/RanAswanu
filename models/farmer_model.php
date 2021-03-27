ee5a6f6173f6b30021d1ffd486e86d6a6450bfe8
ddhanushka <dimuthudhanushka8@gmail.com>
2021-03-27 16:51:15 +0530

    Fix officer/farmerMng error

--- models/farmer_model.php
+++ models/farmer_model.php
@@ -44,13 +44,12 @@ class Farmer_Model extends Model {
 
     /// !!!!!!!!!!!!!!! Handled by OFficer !!!!!!!!!!!!!!!!!!!!!!1
     public function farmerList() {
-        $st = $this->db->prepare("SELECT user.*, group_concat(user_tel.tel_no) AS telNos FROM user JOIN user_tel on user.user_id =user_tel.user_id WHERE user.role = 'farmer' GROUP BY user.user_id");
+        $gs_id =  Session::get('gs_id');
+        $st = $this->db->prepare("SELECT user.*, group_concat(user_tel.tel_no) AS telNos FROM user 
+        JOIN user_tel on user.user_id =user_tel.user_id 
+        WHERE user.role = 'farmer' AND user.gs_id = $gs_id GROUP BY user.user_id");
 
-        // SELECT user.user_name, user.first_name, group_concat(user_tel.tel_no) FROM user JOIN user_tel on user.user_id =user_tel.user_id GROUP BY user.user_id
-        $st->execute(array(
-            ':role' => 'farmer'
-        ));
-        // print_r($st->fetchAll());
+        $st->execute();
 
         return $st->fetchAll(PDO::FETCH_ASSOC);
     }

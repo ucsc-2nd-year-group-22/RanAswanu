39795e4059b8b134f3057a82acbdc02487c649d2
ddhanushka <dimuthudhanushka8@gmail.com>
2021-03-30 10:04:21 +0530

    add report

--- views/officer/reports.php
+++ views/officer/reports.php
@@ -7,73 +7,57 @@
 </div> -->
 
 <div class="main-form">
-    <form action="#" method="post">
+    <form action="<?= URL; ?>/report/generateReport" target="_blank" method="post">
         <div class="row">
             <div class="col-25">
-            <label for="province">Select report type</label>
+                <label for="province">Select report type</label>
             </div>
             <div class="col-75">
-            <select id="province" name="province">
-                <option value="report1">Anual farmer report</option>
-                <option value="report2">Monthly vendor-farmer orders</option>
-                <option value="report3">Farmer report</option>
-                <option value="report3">Damages report</option>
-                <option value="report3">User report</option>
-                <option value="report3">Custom report...</option>
-            </select>
+                <select id="province" name="province">
+                    <option value="report1">report 1</option>
+                    <option value="report2">report 2</option>
+                    <option value="report3">report 3</option>
+                </select>
             </div>
         </div>
         <div class="row">
             <div class="col-25">
-            <label for="dob">From </label>
+                <label for="dob">From </label>
             </div>
             <div class="col-75">
-            <input type="date" id="from" name="from" placeholder="Month/Date/Year ">
+                <input type="date" id="from" name="from" placeholder="Month/Date/Year ">
             </div>
         </div>
         <div class="row">
             <div class="col-25">
-            <label for="dob">To </label>
+                <label for="dob">To </label>
             </div>
             <div class="col-75">
-            <input type="date" id="to" name="to" placeholder="Month/Date/Year ">
+                <input type="date" id="to" name="to" placeholder="Month/Date/Year ">
             </div>
         </div>
         <div class="row">
             <div class="col-25">
-                
-            </div>
-            <div class="col-75">
-                <input type="checkbox" checked="checked" name="remember"> Email report         
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-25">
-                
-            </div>
-            <div class="col-75">
-                <input type="checkbox" checked="checked" name="remember"> Open PDF in new tab         
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-25">
-            
-            </div>
-            <div class="col-75">
-            <button type="Submit"><i class="fas fa-file"></i> Create Report</button>
-            </div>
-        </div>
-    </form>
-</div>
-<div class="main-form">
 
-    <form action="#" method="post">
-        <div class="row">
-            <div class="col-25">
-            
             </div>
             <div class="col-75">
-            <button type="Submit">Email report</button>
+                <input type="checkbox" checked="checked" name="remember"> Email report
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-25">
+
+            </div>
+            <div class="col-75">
+                <input type="checkbox" checked="checked" name="remember"> Open PDF in new tab
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-25">
+
+            </div>
+            <div class="col-75">
+                <button type="Submit"><i class="fas fa-file"></i> Create Report</button>
             </div>
         </div>
     </form>

30a3dee214f3367aeda8ba9a8e90c9a711b9f5de
D. Dhanushka <52200656+DDhanushka@users.noreply.github.com>
2021-04-04 13:35:52 +0530

    Update Email.php

--- libs/Email.php
+++ libs/Email.php
@@ -34,8 +34,8 @@ use PHPMailer\PHPMailer\Exception;
                 $mail->isSMTP();                                            // Send using SMTP
                 $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
                 $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-                $mail->Username   = 'ranaswanu.sys@gmail.com';                     // SMTP username
-                $mail->Password   = 'R@na$wanu';                               // SMTP password
+                $mail->Username   = 'your email here';                     // SMTP username
+                $mail->Password   = 'Your password here';                               // SMTP password
                 $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
                 $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
             
